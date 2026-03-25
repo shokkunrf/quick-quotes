174bab98fa5416145b856308cdbdbef45a732e5b
Sut103 <kou.103.kan@gmail.com>
2026-03-25 14:30:48 +0000

    Add log expiration period configurable via environment variable (#6)

--- db/init/init.sh
+++ db/init/init.sh
@@ -14,7 +14,7 @@ db.createUser({
 
 db.createCollection('$COLLECTION') \n
 
-db.getCollection('$COLLECTION').createIndex({ "time": 1 }, { expireAfterSeconds: 259200 }) \n
+db.getCollection('$COLLECTION').createIndex({ "time": 1 }, { expireAfterSeconds: ${LOG_TTL_SECONDS:-259200} }) \n
 
 EOS
 )
